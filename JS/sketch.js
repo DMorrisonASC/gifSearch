781ee6f06cd9b2ad2bb26544e69a2765be86beb8
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2019-09-04 11:55:16 -0400

    Add files via upload
    Added new feature: When user search something else, the old results are erased

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,12 +1,21 @@
-const images = document.getElementById("images");
-
+// the Div where the GIFs from GIPHY will be appended to.
+const imagesDiv = document.getElementById("imagesDiv");
+// The user input -- the name of GIFs searched. Ex. Cats, Dogs, etc.
 const search = document.getElementById("search");
-
+// The search button for GIFs.
 const submit = document.getElementById("submit");
+// When pressed, it begins searching. 
 submit.addEventListener("click", function () {
+  // Refresh page first to get rid of old search results
+  imagesDiv.innerHTML = "";
   getData(search.value);
+
 });
+
+// Code that uses GIPHY Api
 function getData(query) {
+
+  // fetch data from GIPHY, using the user's input(ex. dogs) to replace word in the link
   fetch(
     "https://api.giphy.com/v1/gifs/search?q=" +
     query +
@@ -17,14 +26,25 @@ function getData(query) {
     })
     .then(function (myJson) {
       renderData(myJson);
-      //   console.log(JSON.stringify(myJson));
+
     });
-}
-function renderData(data) {
-  console.log(data.data);
-  for (let i = 0; i < data.data.length; i++) {
-    const img = document.createElement("img");
-    img.src = data.data[i].images.original.url;
-    images.appendChild(img);
+
+  function renderData(data) {
+    console.log(data.data);
+    // For loop runs as many times as needed to get all GIFs
+    for (let i = 0; i < data.data.length; i++) {
+      // create img element to represent the GIFs
+      const img = document.createElement("img");
+      // give className for css styling
+      img.className = "gifs";
+      // give img url to get the GIFs
+      img.src = data.data[i].images.original.url;
+      // put them into a div
+      imagesDiv.appendChild(img);
+    }
   }
+
 }
+
+
+
